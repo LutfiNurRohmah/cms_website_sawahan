dea2ca81d9ba68fee3c873cb61b891557c75bd46
LutfiNurRohmah <lutfi0036ft.2021@student.uny.ac.id>
2024-12-11 02:14:43 +0700

    feat: update routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\BeritaController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\InfografisController;
+use App\Http\Controllers\KegiatanController;
+use App\Http\Controllers\PotensiController;
+use App\Http\Controllers\ProfilController;
+use App\Http\Controllers\UmkmController;
+use App\Models\ContactInformation;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +21,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/editor');
-});
+// Route::get('/', function () {
+//     return redirect('/editor');
+// });
+
+Route::get('/', [HomeController::class, 'index']);
+
+Route::get('/profil-padukuhan', [ProfilController::class, 'index']);
+
+Route::get('/potensi-padukuhan', [PotensiController::class, 'index']);
+
+Route::get('/potensi-padukuhan/{slug}', [PotensiController::class, 'detail']);
+
+Route::get('/infografis', [InfografisController::class, 'index']);
+
+Route::get('/kegiatan', [KegiatanController::class, 'index']);
+
+Route::get('/kegiatan/{slug}', [KegiatanController::class, 'detail']);
+
+Route::get('/berita', [BeritaController::class, 'index']);
+
+Route::get('/berita/{slug}', [BeritaController::class, 'detail']);
+
+Route::get('/umkm', [UmkmController::class, 'index']);
+
+Route::get('/umkm/{slug}', [UmkmController::class, 'detail']);
+
+
